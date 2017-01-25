c532f2a3dad3533423f194d19fcbfb226b7b5367
sunnymes4 <sunnymes4@gmail.com>
2017-01-25 12:03:29 -0800

    Tested
    Solved

--- Service.js
+++ Service.js
@@ -3,14 +3,16 @@
     //get All Eployee
     this.getEmployees = function () {
         debugger;
-        return $http.get("Home/GetAll");
+        //return $http.get("Home/GetAll");
+        return $http.get("GetAll");
     };
 
     // get Employee By Id
     this.getEmployee = function (employeeID) {
         var response = $http({
             method: "post",
-            url: "Home/getEmployeeByNo",
+            //url: "Home/getEmployeeByNo",
+            url: "getEmployeeByNo",
             params: {
                 id: JSON.stringify(employeeID)
             }
@@ -18,23 +20,13 @@
         return response;
     }
 
-    // Update Employee
-    //this.updateEmp = function (employee) {
-    //    var response = $http({
-    //        method: "post",
-    //        url: "Home/AddUpdateEmployee",
-    //        data: JSON.stringify(employee),
-    //        dataType: "json"
-    //    });
-    //    return response;
-    //}
-
     // Add Employee
     this.AddUpdateEmp = function (employee) {
         debugger;
         var response = $http({
             method: "post",
-            url: "Home/AddUpdateEmployee",
+            //url: "Home/AddUpdateEmployee",
+            url: "AddUpdateEmployee",
             data: JSON.stringify(employee),
             dataType: "json"
         });
@@ -45,10 +37,22 @@
     this.DeleteEmp = function (employeeId) {
         var response = $http({
             method: "post",
-            url: "Home/DeleteEmployee",
+            //url: "Home/DeleteEmployee",
+            url: "DeleteEmployee",
             params: {
                 Id: JSON.stringify(employeeId)
             }
+        });
+        return response;
+    }
+
+    //Login
+    this.UserLogin = function (User) {
+        var response = $http({
+            method: "post",
+            url: "/Login/Login",
+            data: JSON.stringify(User),
+            dataType: "json"
         });
         return response;
     }
